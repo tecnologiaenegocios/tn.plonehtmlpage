17619bcc8b260d018efd7dff03372959ec4bf28f
rcabralc <rcabralc@gmail.com>
2012-06-18 11:26:04 -0300

    Better default view

--- src/tn/plonehtmlpage/html_page.py
+++ src/tn/plonehtmlpage/html_page.py
@@ -1,4 +1,5 @@
 from five import grok
+from Products.CMFCore.utils import getToolByName
 from plone.directives import form
 from tn.ploneformwidget.sourcecode import SourceCodeFieldWidget
 from tn.plonehtmlpage import _
@@ -23,13 +24,25 @@ class IHTMLPageSchema(form.Schema):
     )
 
 
-class ViewRaw(grok.View):
+class ViewRawHTML(grok.View):
     grok.context(IHTMLPageSchema)
     grok.require('zope2.View')
-    grok.name('view-raw')
 
     def render(self):
         return self.context.html
+
+
+class View(grok.View):
+    grok.context(IHTMLPageSchema)
+    grok.require('zope2.View')
+
+    def update(self):
+        portal_url = getToolByName(self.context, 'portal_url')()
+        base_resources_path = portal_url + '/++resource++tn.plonehtmlpage/'
+
+        self.frame_id = u'frame-%s' % self.context.__name__
+        self.iframe_height_js_url = base_resources_path + 'iframe-height.js'
+        self.stylesheet_url = base_resources_path + 'default.css'
 
 
 mode_adapter = widget.StaticWidgetAttribute(u'html',
---
+++ src/tn/plonehtmlpage/resources/iframe-height.js
@@ -0,0 +1,8 @@
+;(function($) {
+  $(function() {
+    $('iframe.html-page-preview').each(function() {
+      var frame = $(this);
+      frame.height(frame.contents().find('html').height());
+    });
+  });
+})(jQuery);
