8a16c79ad5b597b2c3e3075c733a3a014022abba
rcabralc <rcabralc@gmail.com>
2012-06-19 13:29:13 -0300

    Ok, now really, iframe height is done right (I hope...)

--- src/tn/plonehtmlpage/resources/iframe-height.js
+++ src/tn/plonehtmlpage/resources/iframe-height.js
@@ -1,11 +1,8 @@
 ;(function($) {
-  $(function() {
+  $(window).load(function() {
     $('iframe.html-page-preview').each(function() {
       var $frame = $(this);
-      var $doc = $frame.contents();
-      var heightFn = function() { $frame.height($doc.find('html').height()); }
-      $doc.load(heightFn);
-      $doc.ready(heightFn);
+      $frame.height($frame.contents().find('html').height());
     });
   });
 })(jQuery);
